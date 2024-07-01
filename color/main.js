c0e9fb2b452e98d135c7d1ae97b93ba182c3f91d
abdufattoh1205 <abdufattoh1205@gmail.com>
2024-07-01 09:35:24 +0500

    first commit

---
+++ color/main.js
@@ -0,0 +1,7 @@
+
+document.querySelector(".btn").addEventListener('click', function(){
+    const value = document.querySelector("#value").value;
+
+    document.querySelector("body").style.backgroundColor = value
+})
+
---
+++ main.js
@@ -0,0 +1,8 @@
+console.log(5000)
+console.log('i am alpha male don\'t you know tha')\
+alert(log)
+
+var log = 'nigga'
+
+
+
